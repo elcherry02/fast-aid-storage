d380e98486862df05d20f59212c8b1cec36bb362
El Cherry Mae Masibay <111100461+elcherry02@users.noreply.github.com>
2023-05-05 17:45:29 +0800

    added recyclerview for viewing the inputted data

---
+++ app/src/main/java/com/example/fastaidapp/DetailActivity.java
@@ -0,0 +1,32 @@
+package com.example.fastaidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView detailDesc, detailTitle;
+    ImageView detailImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        detailDesc = findViewById(R.id.detailDesc);
+        detailTitle = findViewById(R.id.detailTitle);
+        detailImage = findViewById(R.id.detailImage);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            detailDesc.setText(bundle.getString("Details"));
+            detailTitle.setText(bundle.getString("Title"));
+            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+        }
+    }
+}
--- app/src/main/java/com/example/fastaidapp/MainActivity.java
+++ app/src/main/java/com/example/fastaidapp/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.fastaidapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fab;
+    RecyclerView recyclerView;
+    List<DataClass> dataList;
+    DatabaseReference databaseReference;
+    ValueEventListener eventListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, 1);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        dataList = new ArrayList<>();
+        MyAdapter adapter = new MyAdapter(MainActivity.this, dataList);
+        recyclerView.setAdapter(adapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Fast Aid");
+        dialog.show();
+
+        eventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                dataList.clear();
+                for (DataSnapshot itemSnapshot: snapshot.getChildren()){
+                    DataClass dataClass = itemSnapshot.getValue(DataClass.class);
+                    dataList.add(dataClass);
+                }
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "Database Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
