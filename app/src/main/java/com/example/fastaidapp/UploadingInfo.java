f56be215d23ace1128e023d23229c5100cf25902
El Cherry Mae Masibay <111100461+elcherry02@users.noreply.github.com>
2023-05-05 09:41:53 +0800

    added storage with title, description and language

---
+++ app/src/main/java/com/example/fastaidapp/UploadingInfo.java
@@ -0,0 +1,141 @@
+package com.example.fastaidapp;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class UploadingInfo extends AppCompatActivity {
+
+    ImageView upload_img;
+    Button saveButton;
+    EditText uploadTopic, uploadDesc, uploadLang;
+    String imageURL;
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploading_info);
+
+        upload_img = findViewById(R.id.upload_img);
+        uploadTopic = findViewById(R.id.uploadTopic);
+        uploadDesc = findViewById(R.id.uploadDesc);
+        uploadLang = findViewById(R.id.uploadLang);
+        saveButton = findViewById(R.id.saveButton);
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            uri = data.getData();
+                            upload_img.setImageURI(uri);
+                        } else {
+                            Toast.makeText(UploadingInfo.this, "No Image Selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+        );
+
+        upload_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                activityResultLauncher.launch(photoPicker);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+    }
+
+        public void saveData(){
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Android Images")
+                    .child(uri.getLastPathSegment());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(UploadingInfo.this);
+            builder.setCancelable(false);
+            builder.setView(R.layout.progress_layout);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                    uriTask.addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                Uri urlImage = task.getResult();
+                                imageURL = urlImage.toString();
+                                uploadData();
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    dialog.dismiss();
+                }
+            });
+        }
+
+    public void uploadData(){
+
+            String title = uploadTopic.getText().toString();
+            String desc = uploadDesc.getText().toString();
+            String lang = uploadLang.getText().toString();
+
+            DataClass dataClass = new DataClass(title,desc,lang,imageURL);
+
+            FirebaseDatabase.getInstance().getReference("Fast Aid").child(title)
+                    .setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(UploadingInfo.this, "Saved", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(UploadingInfo.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+}
